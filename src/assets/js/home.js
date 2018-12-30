546138511d2c7c335df8073a9d9d2361480f2a4a
高阳康 <kanggaoyang@greatseacn.com>
2018-12-30 22:22:28 +0800

    首页

---
+++ src/assets/js/home.js
@@ -0,0 +1,10 @@
+$(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true, // 循环模式选项
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    })
+})
