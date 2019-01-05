18b36efa66a9c2a7799ffaad74c813a283371a82
高阳康 <kanggaoyang@greatseacn.com>
2019-01-05 12:58:52 +0800

    辅导回顾

---
+++ src/assets/js/setfont.js
@@ -0,0 +1,15 @@
+//JS监听浏览器文字大小代码
+"use strict";
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = (clientWidth / 3.75) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
