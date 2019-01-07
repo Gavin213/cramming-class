e1fdb7e52c96f2d5332f571d99dee3c3557b47f0
gzh3311 <guozihe@aiyizhuyi.com>
2019-01-07 16:59:07 +0800

    迁移，填充，路由添加

--- database/migrations/2019_01_05_070000_create_course_table.php
+++ database/migrations/2019_01_05_070000_create_course_table.php
@@ -19,6 +19,13 @@ class CreateCourseTable extends Migration
             $table->time('start')->comment('课程开始时间');
             $table->time('end')->comment('课程结束时间');
             $table->string('address')->comment('补课地址');
+            $table->json('teachers')->comment('补课教师');
+            $table->text('content')->nullable()->comment('本次课程全部内容');
+            $table->string('behavior')->nullable()->comment('本次课程学员表现');
+            $table->string('job')->nullable()->comment('课后作业');
+            $table->string('last_time_job')->nullable()->comment('上次作业完成情况');
+            $table->json('img')->nullable()->comment('课程图片');
+            $table->tinyInteger('status')->default(1)->comment('发布课程结束总结 1 保存  2 发布');
             $table->timestamps();
         });
     }
---
+++ database/seeds/CmsuserSeederTable.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CmsuserSeederTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('cmsuser')->insert([
+            'name' => 'GZH',
+            'email' => 'admin@qq.com',
+            'phone' => '18888888888',
+            'status' => '1',
+            'password' => bcrypt('123456'),
+            'remember_token' => str_random(10),
+        ]);
+    }
+}
